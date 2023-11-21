2440106062ba5ef7f04f5f4482781cff9950ba2e
andreictinn <88589252+andreictinn@users.noreply.github.com>
2023-11-21 14:01:57 +0200

    Update milestone_2.py
    Import 'random' function to create a randomiser function for picking out one fruit from word_list

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,8 @@
 import random
 
-word_list = [apple, mango, kiwi, tangerine, orange]
+word_list = ["apple", "mango", "kiwi", "tangerine", "orange"]
+word = random.choice(word_list)
 print(word_list)
+
+
+
